989381095c40441060ff9b3aafe3fbc1e7debcd2
inaraahmadova <tu7r5hs82@code.edu.az>
2024-04-02 00:04:06 +0400

    first commit

---
+++ classencapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace classencapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("inara   ", "   ahmadova", "inara123");
+            user.ChangePassword("Sabir123", "uliyev123");
+
+            Console.WriteLine(user.UserName);
+        }
+    }
+}
+
+
+
